51cb9e5778b0e37d2c8f02e48caf44540dd5dae9
ckoww <hanna22095@gmail.com>
2026-04-29 20:08:27 +0800

    加入專案檔案。

---
+++ frmMediaPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaPlayer
+{
+    public partial class frmMediaPlayer : Form
+    {
+        public frmMediaPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "WMV files (*.wmv)|*.wmv|MP4 files(*.mp4) | *.mp4 | AVI files(*.avi) | *.avi | All files(*.*) | *.* ";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                wmpVideo.URL = ofd.FileName;
+                wmpVideo.Ctlcontrols.stop();
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (wmpVideo.playState == WMPLib.WMPPlayState.wmppsUndefined)
+            {
+                MessageBox.Show("請先選擇影片檔案");
+                return;
+            }
+            wmpVideo.Ctlcontrols.play();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.pause();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.stop();
+        }
+    }
+}
